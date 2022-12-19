ce81b7f57e9f01175ae2a72eff1711ed9d19d76a
SilasTSL <silastaysl@gmail.com>
2022-12-19 17:59:51 +0800

    Change header title to pointer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div className="headerContainer">
-            <h1>THIS IS HEADER</h1>            
+            <h1 className="headerTitle">ModBuddy</h1>
         </div>
     )
 }
