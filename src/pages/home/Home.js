9c03e5a070af8ba64e1b676f6ee11fd03c5903c7
SilasTSL <silastaysl@gmail.com>
2022-12-19 15:53:08 +0800

    Add Header and Footer to Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,15 @@
 import './Home.css';
+import { Header } from '../../components/header/Header';
+import { Footer } from '../../components/footer/Footer';
 
 import React from 'react'
 
 export const Home = () => {
     return (
         <div className="homeContainer">
+            <Header />
             <h1>THIS IS HOME</h1>
+            <Footer />
         </div>
     )
 }
